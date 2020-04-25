be2048570b0037607899473c62fc9c336fb6b07c
itok01 <contact@itok01.com>
2020-04-26 02:45:14 +0900

    Add get()

--- src/messaging/client.rs
+++ src/messaging/client.rs
@@ -1,5 +1,20 @@
 use reqwest::header;
+use serde::Serialize;
 use std::error::Error;
+
+pub async fn get<S: Into<String>, Q: Serialize + ?Sized>(
+    channel_access_token: S,
+    endpoint: S,
+    query: &Q,
+) -> Result<reqwest::Response, Box<dyn Error>> {
+    let client = reqwest::Client::new();
+    Ok(client
+        .get(&endpoint.into())
+        .query(query)
+        .bearer_auth(channel_access_token.into())
+        .send()
+        .await?)
+}
 
 pub async fn post_json<S: Into<String>>(
     channel_access_token: S,
